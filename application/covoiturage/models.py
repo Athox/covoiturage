688beede750ce2fcb81e4bdda53ef595ffa6cca7
anthony <anthony.chan-ou-teung@teamwork.net>
2016-05-10 22:58:26 +0200

    model Driver

--- application/covoiturage/models.py
+++ application/covoiturage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Driver(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nom', unique=True)
+    phone = models.CharField(max_length=10, verbose_name='Telephone')
