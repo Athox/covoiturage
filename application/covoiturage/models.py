3972e48939cbb68651c12eacdd4fd9446d9e57b7
anthony <anthony.chan-ou-teung@teamwork.net>
2016-05-11 09:16:42 +0200

    modif driver model

--- application/covoiturage/models.py
+++ application/covoiturage/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Driver(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Nom', unique=True)
-    phone = models.CharField(max_length=10, verbose_name='Telephone')
+    lastname = models.CharField(max_length=255, verbose_name='Nom')
+    firstname = models.CharField(max_length=255, verbose_name='Prenom')
+    phone = models.CharField(max_length=10, verbose_name='Telephone', blank=True, null=True)
